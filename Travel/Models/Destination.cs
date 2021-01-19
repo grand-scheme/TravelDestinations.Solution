37f868f7f739d1371ac6ab2531e69eb424054087
Eric Stratton <strattonericj@gmail.com>
2021-01-19 13:33:54 -0800

    add validators to destination model
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- Travel/Models/Destination.cs
+++ Travel/Models/Destination.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Travel.Models
 {
   public class Destination
   {
     public int DestinationId { get; set; }
+    [Required]
     public string City { get; set;}
+    [Required]
     public string Country { get; set; }
   }
 }
