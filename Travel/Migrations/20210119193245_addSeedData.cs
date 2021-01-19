90e75c69efd39fa5a78da90e191599de2709ab80
Eric Stratton <strattonericj@gmail.com>
2021-01-19 11:34:17 -0800

    add seed data
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ Travel/Migrations/20210119193245_addSeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class addSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country" },
+                values: new object[] { 1, "London", "England" });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country" },
+                values: new object[] { 2, "Amsterdam", "Netherlands" });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country" },
+                values: new object[] { 3, "Berlin", "Germany" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Destinations",
+                keyColumn: "DestinationId",
+                keyValue: 3);
+        }
+    }
+}
