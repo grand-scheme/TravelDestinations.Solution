23f1640860eff6cc8dc151bd70e9982d159162d6
Eric Stratton <strattonericj@gmail.com>
2021-01-19 14:10:52 -0800

    add new migrations specific to api data only
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>

---
+++ Travel/Migrations/20210119220414_Initial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Destinations",
+                columns: table => new
+                {
+                    DestinationId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Destinations", x => x.DestinationId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Destinations",
+                columns: new[] { "DestinationId", "City", "Country" },
+                values: new object[,]
+                {
+                    { 1, "London", "England" },
+                    { 2, "Amsterdam", "Netherlands" },
+                    { 3, "Berlin", "Germany" },
+                    { 4, "South Pole", "Antartica" },
+                    { 5, "Kyoto", "Japan" },
+                    { 6, "New York City", "USA" },
+                    { 7, "Ho Chi Minh", "Vietnam" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Destinations");
+        }
+    }
+}
