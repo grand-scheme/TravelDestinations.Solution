9abddea52f0e1336a8f3b6383fa79f735df26c2c
Eric Stratton <strattonericj@gmail.com>
2021-01-20 13:30:59 -0800

    add destinations edit view, fix line spacing in model
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- TravelClient/Models/Destination.cs
+++ TravelClient/Models/Destination.cs
@@ -10,6 +10,7 @@ namespace TravelClient.Models
     public int DestinationId { get; set; }
     public string City { get; set; }
     public string Country { get; set; }
+    
 
     public static List<Destination> GetDestinations()
     {
