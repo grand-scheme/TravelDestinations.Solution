e7bbd7e9fe2d50598f5299b8e07d9855cf7a6b6b
Eric Stratton <strattonericj@gmail.com>
2021-01-20 10:02:16 -0800

    update api side controllers
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ Travel/Migrations/20210120175700_addHashSet.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations
+{
+    public partial class addHashSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Reviews",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Reviews");
+        }
+    }
+}
