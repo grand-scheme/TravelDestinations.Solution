b81fa3a777a4d8d74ed4e2f86a712697537f5718
Eric Stratton <strattonericj@gmail.com>
2021-01-20 10:35:36 -0800

    add destinations controller to client side
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ TravelClient/Controllers/DestinationsController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers
+{
+    public class DestinationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var allDestinations = Destination.GetDestinations();
+            return View(allDestinations);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
