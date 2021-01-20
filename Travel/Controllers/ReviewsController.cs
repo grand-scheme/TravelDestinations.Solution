4042ee306c5a62c2d0bb598052e02ac1e8c04925
Eric Stratton <strattonericj@gmail.com>
2021-01-20 13:47:31 -0800

    build out review API controller
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

---
+++ Travel/Controllers/ReviewsController.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Travel.Models;
+
+namespace Travel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private TravelContext _db;
+    
+        public ReviewsController(TravelContext db)
+        {
+            _db = db;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Review>> Get()
+        {
+            var query = _db.Reviews.AsQueryable();
+            return query.ToList();
+            // NOTE: SINCE THIS IS FOR SEARCHING FOR AN INDIVIDUAL ENTRY IN THE API,
+            // THIS SHOULD LATER BE BUILT TO TARGET THE DESTINATION ID
+            // AND RETURN REVIEWS ASSOCIATED W/ THAT DESTINATION
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Review> Get(int id)
+        {
+            return _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Review review)
+        {
+            _db.Reviews.Add(review);
+            _db.SaveChanges();
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Review review)
+        {
+            review.ReviewId = id;
+            _db.Entry(review).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var reviewToDelete = _db.Reviews.FirstOrDefault(entry => entry.ReviewId == id);
+            _db.Reviews.Remove(reviewToDelete);
+            _db.SaveChanges();
+        }
+    }
+}
