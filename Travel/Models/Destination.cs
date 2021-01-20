9bfe390e30898f16ffdc3f08e844ce68bb9752b5
Eric Stratton <strattonericj@gmail.com>
2021-01-20 10:05:32 -0800

    update models
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>
    Co-authored-by: Shannon Grantski <grantski@pm.me>

--- Travel/Models/Destination.cs
+++ Travel/Models/Destination.cs
@@ -1,13 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Travel.Models
 {
   public class Destination
   {
+    public Destination()
+    {
+      this.Reviews = new HashSet<Review>();
+    }
+
     public int DestinationId { get; set; }
     [Required]
     public string City { get; set;}
     [Required]
     public string Country { get; set; }
+
+    public ICollection<Review> Reviews { get; set; }
   }
 }
