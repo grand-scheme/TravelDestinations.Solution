02b0d846798b54ad95aa61f83d4ad3f96238e761
Eric Stratton <strattonericj@gmail.com>
2021-01-21 10:26:19 -0800

    add numerous migrations to get idenity tables in database - succesful
    Co-authored-by: Shannon Grantski <grantski@pm.me>
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- Travel/Migrations/20210121182427_plzFixAuthentication.Designer.cs
+++ Travel/Migrations/20210121182427_plzFixAuthentication.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Travel.Models;
 
 namespace Travel.Migrations
 {
     [DbContext(typeof(TravelContext))]
-    partial class TravelContextModelSnapshot : ModelSnapshot
+    [Migration("20210121182427_plzFixAuthentication")]
+    partial class plzFixAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
